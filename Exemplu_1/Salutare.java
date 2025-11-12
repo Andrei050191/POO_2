960378adce18ecee199e8ee007776aec9c84fc1f
Andrei <ungureanuandrei163303@gmail.com>
2025-11-12 10:57:20 +0200

    scaner pentru input de la tastatura

--- Exemplu_1/Salutare.java
+++
@@ -1,9 +0,0 @@
-package Exemplu_1;
-public class Salutare {
-    public static void main(String[] args) {
-        double number1, number2;
-        number1 = 45;
-        number2 = 34;
-        System.out.println("Sum is " + (number1 + number2));
-    }
-}
