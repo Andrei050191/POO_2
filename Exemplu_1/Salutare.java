f8dbe93b9b45b957e21ac2c13f0a0f480c6659a6
Andrei <ungureanuandrei163303@gmail.com>
2025-11-12 09:25:26 +0200

    suma 2 numere

---
+++ Exemplu_1/Salutare.java
@@ -0,0 +1,9 @@
+package Exemplu_1;
+public class Salutare {
+    public static void main(String[] args) {
+        double number1, number2;
+        number1 = 45;
+        number2 = 34;
+        System.out.println("Sum is " + (number1 + number2));
+    }
+}
